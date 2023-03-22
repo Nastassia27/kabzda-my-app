0d87260a49d9cb344ddc1059acef64e41c8f607a
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-22 23:23:51 +0300

    set state to switch on off

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 type OnOffType = {
-    isOn: boolean
+    onClick: (value: boolean)=>void
+    value: boolean
 }
 
 
 export function OnOff(props: OnOffType) {
+
     const OnStyle = {
         width: '60px',
         height: "30px",
         border: "1px solid black",
         display: "inline-block",
-        padding:"2px",
-        marginRight:"5px",
-        backgroundColor: props.isOn ? "green" : "white"
+        padding: "2px",
+        marginRight: "5px",
+        backgroundColor: props.value ? "green" : "white"
     }
     const OffStyle = {
         width: '60px',
         height: "30px",
         border: "1px solid black",
         display: "inline-block",
-        padding:"2px",
-        backgroundColor: props.isOn ? "white" : "red"
+        padding: "2px",
+        backgroundColor: props.value ? "white" : "red"
     }
 
     const IndicatorStyle = {
@@ -30,14 +32,14 @@ export function OnOff(props: OnOffType) {
         borderRadius: "15px",
         border: "1px solid black",
         display: "inline-block",
-marginLeft:"5px",
-        backgroundColor: props.isOn ? "green" : "red"
+        marginLeft: "5px",
+        backgroundColor: props.value ? "green" : "red"
     }
 
     return (
         <div>
-            <div style={OnStyle}>On</div>
-            <div style={OffStyle}>Off</div>
+            <div style={OnStyle} onClick={()=>{props.onClick(!props.value)}}>On</div>
+            <div style={OffStyle} onClick={()=>{props.onClick(!props.value)}}>Off</div>
             <div style={IndicatorStyle}></div>
         </div>
     )
