297f42c91eda6058a090f6b013c957de8c45a9ea
Nastassia27 <anastasia.derugo@gmail.com>
2023-04-08 22:14:05 +0300

    change onOff stories

--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -1,21 +1,21 @@
 import React, {useState} from "react";
-import {Button} from "../../stories/Button";
-import {Rating, RatingValueType} from "./Rating";
+
+import {OnOff} from "./OnOff";
+import {Rating, RatingValueType} from "../Rating/Rating";
 
 
 export default{
-    title: 'Rating stories',
-    component: Rating,
+    title: 'OnOff stories',
+    component: OnOff,
 };
 
-export const EmptyStars = ()=> <Rating value={0} onClick={x=>x}/>
-export const Rating1 = ()=> <Rating value={1} onClick={x=>x}/>
-export const Rating2 = ()=> <Rating value={2} onClick={x=>x}/>
-export const Rating3 = ()=> <Rating value={3} onClick={x=>x}/>
-export const Rating4 = ()=> <Rating value={4} onClick={x=>x}/>
-export const Rating5 = ()=> <Rating value={5} onClick={x=>x}/>
-export const RatingChanging = ()=>{
-    const [rating, setRating]= useState<RatingValueType>(5)
+export const OnMode = ()=> <OnOff value={true} onClick={x=>x}/>
+export const OffMode = ()=> <OnOff value={false} onClick={x=>x}/>
 
-    return <Rating value={rating} onClick={setRating}/>
+export const ModeChanging = ()=>{
+    const [value, setValue]=useState<boolean>(true);
+    return <OnOff onClick={setValue} value={value}/>
+
+
+
 }
